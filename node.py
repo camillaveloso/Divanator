0ee4f1303a73d1f623eae3d31dd1c72207828330
Camilla Veloso <166400880+camillaveloso@users.noreply.github.com>
2026-03-16 21:06:15 -0300

    Add Node class with initialization and leaf check

--- node.py
+++ node.py
@@ -1,1 +1,10 @@
+class Node:
+    def __init__(self, question=None, answer=None, candidates=None):
+        self.question = question
+        self.answer = answer
+        self.candidates = candidates if candidates is not None else []
+        self.yes = None
+        self.no = None
 
+    def is_leaf(self):
+        return self.answer is not None
